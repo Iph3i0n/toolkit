2d1f42341f2227a5cf6fd846c1203277ce1c35dd
Paul Popat <paul@popatux.co.uk>
2023-10-10 19:01:17 +0000

    Added moulding tin

---
+++ lib/fs-db/src/i-directory.ts
@@ -0,0 +1,20 @@
+import * as MT from "@ipheion/moulding-tin";
+
+export type Schema = Record<string, MT.ISerialiseable<unknown>>;
+
+export type StateWriter<TSchema extends Schema> = Partial<{
+  [TKey in keyof TSchema]: Record<
+    string,
+    MT.Serialised<TSchema[TKey]> | undefined
+  >;
+}>;
+
+export type StateReader<TSchema extends Schema> = {
+  [TKey in keyof TSchema]: Record<string, MT.Serialised<TSchema[TKey]>> &
+    Iterable<[string, MT.Serialised<TSchema[TKey]>]>;
+};
+
+export default interface IDirectory<TSchema extends Schema> {
+  readonly Model: StateReader<TSchema>;
+  Write(data: StateWriter<TSchema>): void;
+}
---
+++ lib/fs-db/src/mod.test.ts
@@ -0,0 +1,93 @@
+import { ASCII, Struct } from "@ipheion/moulding-tin";
+import { Directory, Schema } from "./mod";
+import Fs from "fs/promises";
+
+const TEST_DIR = "./test-data";
+
+function Test<T extends Schema>(
+  name: string,
+  init: T,
+  handler: (dir: Directory<T>) => void
+) {
+  test(name, async () => {
+    try {
+      await Fs.rmdir(TEST_DIR, { recursive: true });
+    } catch {
+      // We do not care if the directory does not exist
+    }
+
+    const db = new Directory(init, TEST_DIR);
+
+    handler(db);
+  });
+}
+
+Test(
+  "Creates a basic state",
+  {
+    item_1: new Struct({
+      an_item: new ASCII(),
+    }),
+  },
+  (dir) => {
+    dir.Write({
+      item_1: {
+        test_id: {
+          an_item: "Hello world",
+        },
+      },
+    });
+
+    expect(dir.Model.item_1.test_id).toEqual({ an_item: "Hello world" });
+  }
+);
+
+Test(
+  "Can iterate over state",
+  {
+    item_1: new Struct({
+      an_item: new ASCII(),
+    }),
+  },
+  (dir) => {
+    dir.Write({
+      item_1: {
+        test_id_1: {
+          an_item: "This is the first item",
+        },
+        test_id_2: {
+          an_item: "This is the second item",
+        },
+        test_id_3: {
+          an_item: "This is the third item",
+        },
+        test_id_4: {
+          an_item: "This is the fourth item",
+        },
+      },
+    });
+
+    let i = 0;
+    for (const [key, value] of dir.Model.item_1) {
+      i++;
+      switch (i) {
+        case 1:
+          expect(key).toEqual("test_id_1");
+          expect(value).toEqual({ an_item: "This is the first item" });
+          break;
+        case 2:
+          expect(key).toEqual("test_id_2");
+          expect(value).toEqual({ an_item: "This is the second item" });
+          break;
+        case 3:
+          expect(key).toEqual("test_id_3");
+          expect(value).toEqual({ an_item: "This is the third item" });
+          break;
+        case 4:
+          expect(key).toEqual("test_id_4");
+          expect(value).toEqual({ an_item: "This is the fourth item" });
+          break;
+      }
+    }
+  }
+);
