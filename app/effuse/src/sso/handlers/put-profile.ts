6f7282b262a472ed62d22b307efe3cc68eadd6f8
Paul Popat <paul@popatux.co.uk>
2024-06-21 12:46:16 +0000

    Added update profile

---
+++ app/effuse/src/sso/handlers/put-profile.ts
@@ -0,0 +1,77 @@
+import { IProfilePicture } from "$i/i-profile-picture";
+import { NewProfilePicture } from "$sso/b/integrations/profile-picture";
+import { NewAuthService } from "$sso/b/services/auth-service";
+import { AuthService } from "$sso/s/auth-service";
+import { Handler, Result, State } from "$sso/server";
+import {
+  EmptyResponse,
+  HttpMethod,
+  JsonResponse,
+  PureRequest,
+} from "@ipheion/puristee";
+import { IsObject, IsString } from "@ipheion/safe-type";
+
+export default class PutProfile extends Handler {
+  readonly #auth_service: AuthService;
+  readonly #profile_picture: IProfilePicture;
+
+  constructor(
+    state: State,
+    auth_service: AuthService = NewAuthService(state),
+    profile_picture: IProfilePicture = NewProfilePicture(state)
+  ) {
+    super(state);
+    this.#auth_service = auth_service;
+    this.#profile_picture = profile_picture;
+  }
+
+  readonly Method = HttpMethod.Put;
+  readonly Url = "/api/v1/user/profile";
+
+  async Process(request: PureRequest) {
+    const body = request.Body(
+      IsObject({
+        UserName: IsString,
+        Biography: IsString,
+        Picture: IsObject({
+          Base64Data: IsString,
+          MimeType: IsString,
+        }),
+      })
+    );
+
+    if (!body) return new Result(new EmptyResponse("BadRequest"));
+
+    const [user, user_id] = await this.#auth_service.GetAdminUser(request);
+    if (!user) return new Result(new EmptyResponse("NotFound"));
+
+    await this.#profile_picture.SavePicture(
+      user_id,
+      body.Picture.MimeType,
+      body.Picture.Base64Data
+    );
+
+    return new Result(
+      new JsonResponse("Ok", {
+        UserId: user_id,
+        Email: user.email,
+        UserName: user.username,
+        Biography: body.Biography,
+        RegisteredAt: user.registered_at.toISOString(),
+        LastSignIn: user.last_sign_in.toISOString(),
+        Servers: user.servers.map((s) => ({
+          Url: s.url,
+          JoinedAt: s.joined_at.toISOString(),
+        })),
+      }),
+      {
+        users: {
+          [user_id]: {
+            ...user,
+            biography: body.Biography,
+          },
+        },
+      }
+    );
+  }
+}
