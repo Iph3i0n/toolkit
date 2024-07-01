5ccc01099d97ab7dea8970cf2056066b21381126
Paul Popat <paul@popatux.co.uk>
2024-07-01 09:14:22 +0000

    Added bootstrapping utils

---
+++ app/effuse/src/local/bootstrap/integrations/parameter-client.ts
@@ -0,0 +1,7 @@
+import { EnvVarParameterClient } from "integrations/env-var-parameter-client";
+import { IParameterClient } from "integrations/i-parameter-client";
+import { b } from "../common";
+
+export const NewParameterClient = b(
+  (): IParameterClient => new EnvVarParameterClient()
+);
