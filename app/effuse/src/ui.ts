874236e51a337329bd513a2c33a41ec35cc97026
Paul Popat <paul@popatux.co.uk>
2024-07-09 12:46:38 +0000

    Added the profile

--- app/effuse/src/ui.ts
+++ app/effuse/src/ui.ts
@@ -2,3 +2,4 @@ import "./user-interface/components/effuse-start.std";
 import "./user-interface/components/effuse-server-list.std";
 import "./user-interface/components/effuse-server-list-item.std";
 import "./user-interface/components/effuse-join-server.std";
+import "./user-interface/components/effuse-profile.std";
