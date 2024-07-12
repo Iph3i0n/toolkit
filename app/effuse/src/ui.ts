de2d428f6da29aa0f35ab2eb3519b93c4542e6a3
Paul Popat <paul@popatux.co.uk>
2024-07-12 09:42:54 +0000

    Better layout

--- app/effuse/src/ui.ts
+++ app/effuse/src/ui.ts
@@ -1,5 +1,5 @@
 import "./user-interface/components/effuse-start.std";
-import "./user-interface/components/effuse-server-list.std";
+import "./user-interface/components/effuse-config.std";
 import "./user-interface/components/effuse-server-list-item.std";
 import "./user-interface/components/effuse-join-server.std";
 import "./user-interface/components/effuse-profile.std";
