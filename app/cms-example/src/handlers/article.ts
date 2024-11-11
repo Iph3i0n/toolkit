c3c464c2fc5e6ced36cee552700a3d422975f208
Paul Popat <paul@popatux.co.uk>
2024-11-11 18:51:32 +0000

    Added example stuff

--- app/cms-example/src/handlers/article.ts
+++ app/cms-example/src/handlers/article.ts
@@ -1,15 +1,16 @@
 import { HttpMethod, PureRequest } from "@ipheion/puristee";
+import { IsString } from "@ipheion/safe-type";
 import { EjsResponse } from "response";
 import { Handler } from "server";
 
 export default class Home extends Handler {
   readonly Method = HttpMethod.Get;
-  readonly Url = "/";
+  readonly Url = "/blog/:article";
 
   async Process(request: PureRequest) {
-    return new EjsResponse("home", {
-      uitext: await this.Page("home-page"),
-      blogs: await this.List("blogs"),
+    const { article } = request.Parameters({ article: IsString });
+    return new EjsResponse("article", {
+      uitext: await this.Entry("blogs", article),
     });
   }
 }
--- app/cms-example/src/server.ts
+++ app/cms-example/src/server.ts
@@ -1,6 +1,7 @@
 import Path from "node:path";
 import CreateServer from "@ipheion/puristee";
 import { StateReader } from "@ipheion/fs-db";
+import Axios from "axios";
 
 const InitalState = {};
 
@@ -10,9 +11,22 @@ export type State = StateReader<typeof InitalState>;
 
 const Server = CreateServer(Path.resolve(DataDir), InitalState, {});
 
-export const Handler = Server.Handler;
+export abstract class Handler extends Server.Handler {
+  async Page(name: string) {
+    const result = await Axios.get("http://localhost:1337/" + name);
+    return result.data;
+  }
 
-export type Handler = typeof Server.Handler;
+  async List(area: string) {
+    const result = await Axios.get("http://localhost:1337/" + area);
+    return result.data;
+  }
+
+  async Entry(area: string, name: string) {
+    const result = await Axios.get(`http://localhost:1337/${area}/${name}`);
+    return result.data;
+  }
+}
 
 export const Result = Server.Response;
 
